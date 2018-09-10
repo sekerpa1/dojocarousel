256029ceb39bf0488194c949af2d570a8e2ccd5d
sekerpa1 <pavol.sekeres@gmail.com>
2018-09-10 21:39:10 +0200

    first commit

---
+++ DojoCarousel/wwwroot/carousel.js
@@ -0,0 +1,7 @@
+﻿require([
+    'dojo/dom',
+    'dojo/dom-construct'
+], function (dom, domConstruct) {
+    var greetingNode = dom.byId('greeting');
+    domConstruct.place('<i> Dojo!</i>', greetingNode);
+});
